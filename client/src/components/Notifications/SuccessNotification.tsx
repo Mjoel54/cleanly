df4d3ab4a3d1eb03ef7e23cd9c5a52920f14d042
Mitch <mitchjoelklein@hotmail.com>
2025-02-10 09:40:28 +1100

    Success notification style updated

--- client/src/components/Notifications/SuccessNotification.tsx
+++ client/src/components/Notifications/SuccessNotification.tsx
@@ -22,24 +22,24 @@ export default function SuccessNotification({
     >
       <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
         <Transition show={show}>
-          <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-green-50 ring-1 shadow-lg ring-black/5 transition data-closed:opacity-0 data-enter:transform data-enter:duration-300 data-enter:ease-out data-closed:data-enter:translate-y-2 data-leave:duration-100 data-leave:ease-in data-closed:data-enter:sm:translate-x-2 data-closed:data-enter:sm:translate-y-0">
+          <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-green-700 ring-1 shadow-lg ring-black/5 transition data-closed:opacity-0 data-enter:transform data-enter:duration-300 data-enter:ease-out data-closed:data-enter:translate-y-2 data-leave:duration-100 data-leave:ease-in data-closed:data-enter:sm:translate-x-2 data-closed:data-enter:sm:translate-y-0">
             <div className="p-4">
               <div className="flex items-start">
                 <div className="shrink-0">
                   <CheckCircleIcon
                     aria-hidden="true"
-                    className="size-6 text-green-600"
+                    className="size-6 text-white"
                   />
                 </div>
                 <div className="ml-3 w-0 flex-1 pt-0.5">
-                  <p className="text-sm font-medium text-gray-900">{title}</p>
+                  <p className="text-sm font-medium text-white">{title}</p>
                   {/* <p className="mt-1 text-sm text-gray-500">{message}</p> */}
                 </div>
                 <div className="ml-4 flex shrink-0">
                   <button
                     type="button"
                     onClick={onClose}
-                    className="inline-flex rounded-md bg-green-50 text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden cursor-pointer"
+                    className="inline-flex rounded-md bg-green-700 text-white hover:bg-green-800 cursor-pointer p-1"
                   >
                     <span className="sr-only">Close</span>
                     <XMarkIcon aria-hidden="true" className="size-5" />
--- client/src/components/Rooms/AddRoomForm.tsx
+++ client/src/components/Rooms/AddRoomForm.tsx
@@ -60,7 +60,6 @@ export default function AddTaskForm() {
               type="text"
               value={roomName}
               onChange={handleChange}
-              placeholder="room name"
               aria-label="Room"
               className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
             />
@@ -75,7 +74,7 @@ export default function AddTaskForm() {
         <SuccessNotification
           show={showNotification}
           onClose={() => setShowNotification(false)}
-          title="Room added!"
+          title="Room added"
         />
       </div>
     </div>
