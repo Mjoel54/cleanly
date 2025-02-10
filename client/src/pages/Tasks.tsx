3486198aa152a01c1521d011c7f51ae8ebc2e6eb
Mitch <mitchjoelklein@hotmail.com>
2025-02-10 20:13:31 +1100

    TasksList component

--- client/src/pages/Tasks.tsx
+++ client/src/pages/Tasks.tsx
@@ -1,10 +1,12 @@
 import AddTasksForm from "../components/Tasks/AddTaskForm";
+import TasksList from "../components/Tasks/TasksList";
 
 export default function Tasks() {
   return (
     <>
       <h1>Tasks</h1>
       <AddTasksForm />
+      <TasksList />
     </>
   );
 }
