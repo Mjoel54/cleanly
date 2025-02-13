38743aad09d0c695a19bbd5a3fafae829f3d7f99
Mitch <mitchjoelklein@hotmail.com>
2025-02-13 11:43:20 +1100

    deleteAllTasks button

--- client/src/components/Tasks/TaskPageHeader.tsx
+++ client/src/components/Tasks/TaskPageHeader.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import AddTaskButton from "./AddTaskButton";
 import AddTaskForm from "./AddTaskForm";
+import { DELETE_ALL_TASKS, GET_TASKS, GET_ROOMS } from "../../utils/api/index";
+import { useMutation } from "@apollo/client";
+import successNotification from "../../utils/successNotification";
 
 export default function TaskPageHeader() {
   const [showForm, setShowForm] = useState(false);
@@ -13,14 +16,48 @@ export default function TaskPageHeader() {
     setShowForm(false);
   };
 
+  const handleDeleteAllTasksButtonclick = (e: any) => {
+    e.preventDefault();
+    deleteAllTasks();
+  };
+
+  const [deleteAllTasks, { loading: isDeleting }] = useMutation(
+    DELETE_ALL_TASKS,
+    {
+      refetchQueries: [{ query: GET_TASKS }, { query: GET_ROOMS }],
+      awaitRefetchQueries: true,
+      onCompleted: () => {
+        successNotification("Tasks deleted");
+      },
+    }
+  );
+
   return (
     <>
-      <div className="flex items-center gap-4">
-        <AddTaskButton onClick={handleButtonClick} />
-        {/* <h2 className="text-2xl font-bold text-gray-900 sm:text-3xl sm:tracking-tight">
-          Tasks
-        </h2> */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <AddTaskButton onClick={handleButtonClick} />
+          {/* <h2 className="text-2xl font-bold text-gray-900 sm:text-3xl sm:tracking-tight">
+            Tasks
+          </h2> */}
+          <div className="px-4 py-2 bg-gray-100 rounded-md hover:bg-gray-200 cursor-pointer">
+            Filter Tasks
+          </div>
+          <div className="px-4 py-2 bg-gray-100 rounded-md hover:bg-gray-200 cursor-pointer">
+            Sort Tasks
+          </div>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <button
+            onClick={handleDeleteAllTasksButtonclick}
+            className="px-4 py-2 bg-gray-100 rounded-md hover:bg-gray-200 cursor-pointer"
+          >
+            Delete All Tasks
+          </button>
+        </div>
       </div>
+
       <hr className="border-t border-gray-300 mt-6" />
       {showForm && <AddTaskForm onClose={closeForm} />}
     </>
