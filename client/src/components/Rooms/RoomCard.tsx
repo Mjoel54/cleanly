257dfcfc3bc951b3f8c07baa6f3b4b18a13cfd59
Mitch <mitchjoelklein@hotmail.com>
2025-02-19 15:53:55 +1100

    Added placeholder room icon

--- client/src/components/Rooms/RoomCard.tsx
+++ client/src/components/Rooms/RoomCard.tsx
@@ -1,6 +1,7 @@
 // import RoomActionsDropdown from "./RoomActionsDropdown";
 // import EditableRoomName from "./EditableRoomName";
 import RoomActions from "./RoomActions";
+import fridgeIcon from "../../images/fridge.svg";
 
 interface RoomCardProps {
   roomId: string;
@@ -14,11 +15,19 @@ export default function RoomCard({
   taskCount,
 }: RoomCardProps) {
   return (
-    <div className="bg-white shadow-sm sm:rounded-lg my-5">
+    <div className="bg-white shadow-sm sm:rounded-lg my-5 ring-1 ring-black/5">
       <div className="px-4 py-5 sm:p-6">
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-between sm:items-center sm:space-y-0">
           {/* Left section: Room name + Task count */}
-          <div className="flex w-full sm:w-auto justify-between sm:justify-start sm:space-x-6">
+          <div className="flex w-full sm:w-auto justify-between items-center sm:justify-start sm:space-x-6">
+            <div className="ring-1 shadow-sm ring-black/5 rounded-md p-2 bg-orange-500">
+              {/* <p className="text-white">icon</p> */}
+              <img
+                src={fridgeIcon}
+                alt="fridge"
+                className="w-8 h-8 brightness-0 invert"
+              />
+            </div>
             <div className="min-w-[150px]">
               {/* Adjust `min-w-[150px]` to a comfortable width */}
               <h3 className="font-semibold">{roomName}</h3>
