f753f4ee0463a30cf21e708f0cf89e9161e6411f
Mitch <mitchjoelklein@hotmail.com>
2025-02-21 14:34:46 +1100

    Complete and Incomplete task lists

--- client/src/components/Tasks/TaskList.tsx
+++ client/src/components/Tasks/TaskList.tsx
@@ -3,10 +3,17 @@ import TaskItem from "./TaskItem";
 // import types
 import { TaskResponse } from "../../interfaces/Task";
 
-export default function TaskList({ completedTasks, incompleteTasks }) {
+interface TaskListProps {
+  completedTasks: TaskResponse[];
+  incompleteTasks: TaskResponse[];
+}
+
+export default function TaskList({
+  completedTasks,
+  incompleteTasks,
+}: TaskListProps) {
   return (
     <div>
-      <p>Incomplete Tasks:</p>
       {incompleteTasks.map((task: TaskResponse) => (
         <TaskItem
           key={task._id}
@@ -18,19 +25,24 @@ export default function TaskList({ completedTasks, incompleteTasks }) {
           dueDate={task.dueDate}
         />
       ))}
-      <hr />
-      <p>Completed Tasks:</p>
-      {completedTasks.map((task: TaskResponse) => (
-        <TaskItem
-          key={task._id}
-          taskName={task.name}
-          taskID={task._id}
-          room={task.room.name}
-          status={task.status}
-          // dueDate={dayjs.unix(task.dueDate).format("DD/MM/YYYY")}
-          dueDate={task.dueDate}
-        />
-      ))}
+      {completedTasks.length > 0 ? (
+        <>
+          <hr className="border-t border-gray-300 mt-6" />
+          {completedTasks.map((task: TaskResponse) => (
+            <TaskItem
+              key={task._id}
+              taskName={task.name}
+              taskID={task._id}
+              room={task.room.name}
+              status={task.status}
+              // dueDate={dayjs.unix(task.dueDate).format("DD/MM/YYYY")}
+              dueDate={task.dueDate}
+            />
+          ))}
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
