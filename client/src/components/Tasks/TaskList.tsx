43983e88e912a9bdd6faac9a6b83fba328713cc7
Mitch <mitchjoelklein@hotmail.com>
2025-03-27 20:35:43 +1100

    reconfigured React architecture of TaskPage

--- client/src/components/Tasks/TaskList.tsx
+++ client/src/components/Tasks/TaskList.tsx
@@ -1,18 +1,16 @@
 import TaskItem from "./TaskItem";
 import CompletedTaskItem from "./CompletedTaskItem";
-
-// import types
 import { TaskResponse } from "../../interfaces/Task";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllTasks } from "../../redux/actions/taskActions";
+import { RootState, AppDispatch } from "../../redux/store";
 
-interface TaskListProps {
-  completedTasks: TaskResponse[];
-  incompleteTasks: TaskResponse[];
-}
+// FilteredTasks component handles the filtering logic
+function FilteredTasks({ tasks }: { tasks: TaskResponse[] }) {
+  const completedTasks = tasks.filter((task) => task.isCompleted === true);
+  const incompleteTasks = tasks.filter((task) => task.isCompleted === false);
 
-export default function TaskList({
-  completedTasks,
-  incompleteTasks,
-}: TaskListProps) {
   return (
     <div>
       {incompleteTasks.map((task: TaskResponse) => (
@@ -21,7 +19,6 @@ export default function TaskList({
           taskName={task.name}
           taskID={task._id}
           room={task.room.name}
-          // dueDate={dayjs.unix(task.dueDate).format("DD/MM/YYYY")}
           dueDate={task.dueDate}
         />
       ))}
@@ -29,16 +26,12 @@ export default function TaskList({
         <>
           {incompleteTasks.length > 0 ? (
             <hr className="border-t border-gray-300 mt-6" />
-          ) : (
-            <> </>
-          )}
+          ) : null}
           {completedTasks.length > 0 ? (
             <h2 className="text-lg font-semibold text-gray-600 my-4 ml-1">
               Completed
             </h2>
-          ) : (
-            <></>
-          )}
+          ) : null}
 
           {completedTasks.map((task: TaskResponse) => (
             <CompletedTaskItem
@@ -46,13 +39,28 @@ export default function TaskList({
               taskName={task.name}
               taskID={task._id}
               room={task.room.name}
-              // dueDate={dayjs.unix(task.dueDate).format("DD/MM/YYYY")}
             />
           ))}
         </>
-      ) : (
-        <> </>
-      )}
+      ) : null}
     </div>
   );
+}
+
+export default function TaskList() {
+  const dispatch = useDispatch<AppDispatch>();
+  const {
+    items: tasks,
+    status,
+    error,
+  } = useSelector((state: RootState) => state.reducer.tasks);
+
+  useEffect(() => {
+    dispatch(fetchAllTasks());
+  }, [dispatch]);
+
+  if (status === "loading") return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return <FilteredTasks tasks={tasks} />;
 }
